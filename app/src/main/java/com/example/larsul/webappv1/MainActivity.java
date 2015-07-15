4acfd2d84b297e20776b5c7be8d1a0598195f41f
Leonardo Urrego <trecetp@gmail.com>
2015-07-15 14:12:28 -0500

    commit inicial

---
+++ app/src/main/java/com/example/larsul/webappv1/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.larsul.webappv1;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        // INI AGREGADO
+        CookieSyncManager.createInstance(this);
+        CookieSyncManager.getInstance().startSync();
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        // Activamos Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        mWebView.setWebContentsDebuggingEnabled(true);
+        // Url que carga la app (webview)
+        mWebView.loadUrl("file:///android_asset/index.html");
+        // Forzamos el webview para que abra los enlaces internos dentro de la la APP
+        mWebView.setWebViewClient(new WebViewClient());
+        // Forzamos el webview para que abra los enlaces externos en el navegador
+        // mWebView.setWebViewClient(new MyAppWebViewClient());
+        // FIN AGREGADO
+    }
+
+    // INI AGREGADO
+    @Override
+    // Detectar cuando se presiona el botón de retroceso
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    // FIN AGREGADO
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CookieSyncManager.getInstance().stopSync();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        CookieSyncManager.getInstance().sync();
+    }
+}
